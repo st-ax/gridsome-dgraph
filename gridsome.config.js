850a8523bedca8addd36975b0ebbdcc13f962465
gotjosh <gitlab@begreen.nu>
2020-08-15 13:01:45 +0100

    updates cleanups and config comments

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,9 +5,10 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 const merge = require('webpack-merge')
-const WorkerPlugin = require('worker-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const CompressionPlugin = require('compression-webpack-plugin');
+const webpack = require('webpack')
+const WorkerPlugin = require('worker-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
   siteName: 'Gridsome JSON editor',
@@ -17,11 +18,17 @@ module.exports = {
       output: {
         globalObject: "(typeof self!='undefined'?self:global)"
       },
+      // node: { crypto: true, stream: true, buffer: true },
       plugins: [
         new WorkerPlugin(),
         new CompressionPlugin(),
-        new BundleAnalyzerPlugin()
-      ]
+        // new webpack.IgnorePlugin(/crypto/), // attempts to get gridsome build ssr working - seemed to get past the errors but then failed when coreui looked for document
+        // new webpack.IgnorePlugin(/http/),
+        // new BundleAnalyzerPlugin(),
+      ],
+      // chainWebpack: config => {
+      //   config.plugin().use(new webpack.IgnorePlugin(/crypto/))
+      // },
     }, config)
   }
 }
